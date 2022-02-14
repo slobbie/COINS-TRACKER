2493fc0116406040004d6e2cec4ad50036ce42fb
slobbie <haeru9410@gmail.com>
2022-02-15 01:59:41 +0900

    coin api 연결 및 conis layout 구현

--- src/router/Coin.tsx
+++ src/router/Coin.tsx
@@ -1,12 +1,48 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface RouterState {
+  name: string;
+}
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 const Coin = () => {
+  const [loading, setLoading] = useState<boolean>(true);
   const { coinId } = useParams<string>();
+  const location = useLocation();
+
+  const name = location.state as RouterState;
 
   return (
-    <div>
-      <h1>coin: {coinId}</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>{name || 'Loading...'}</Title>
+      </Header>
+      {loading ? <Loader> Loading...</Loader> : null}
+    </Container>
   );
 };
 
--- src/router/Coins.tsx
+++ src/router/Coins.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from 'react';
+import { replaceEqualDeep } from 'react-query/types/core/utils';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -21,9 +25,10 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 20px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -32,53 +37,64 @@ const Coin = styled.li`
   }
 `;
 
-const coins = [
-  {
-    id: 'btc-bitcoin',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'eth-ethereum',
-    name: 'Ethereum',
-    symbol: 'ETH',
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'hex-hex',
-    name: 'HEX',
-    symbol: 'HEX',
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: 'token',
-  },
-];
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
   font-size: 48px;
 `;
 
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
 const Coins = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>coins</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>'loading...'</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={coin.name}>
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 };
