8ced97695839fd25f950b47376a1dc0679d8112f
slobbie <haeru9410@gmail.com>
2022-02-17 23:18:32 +0900

    ApexChart 연결하여 데이터 시각화

--- src/router/Chart.tsx
+++ src/router/Chart.tsx
@@ -1,15 +1,71 @@
 import { useQuery } from 'react-query';
 import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
+import Price from './Price';
+
+// https://apexcharts.com
 
 interface ChartProps {
   coinId: string;
 }
 
+interface Ihistorical {
+  time_open: string;
+  time_close: string;
+  open: Number;
+  high: Number;
+  low: Number;
+  close: Number;
+  volume: Number;
+  market_cap: Number;
+}
+
 const Chart = ({ coinId }: ChartProps) => {
-  const { isLoading, data } = useQuery(['ohlcv', coinId], () =>
+  const { isLoading, data } = useQuery<Ihistorical[]>(['ohlcv', coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        'Loading chart'
+      ) : (
+        <ApexChart
+          type='line'
+          series={[
+            {
+              name: 'Price',
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              background: 'transparent',
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: { show: false },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+            },
+            stroke: {
+              curve: 'smooth',
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
